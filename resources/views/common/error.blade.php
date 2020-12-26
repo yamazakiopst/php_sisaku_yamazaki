5610ed5ec9f1da6730f3b42c09a8705a4ac847ba
yamazaki@OPST <yamazaki.kouki@opst.co.jp>
2020-12-26 23:24:22 +0900

    エラー画面修正

--- resources/views/common/error.blade.php
+++ resources/views/common/error.blade.php
@@ -1,29 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('common.layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'PHP_sisaku_yamazaki') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
-<body>
-    <label>予期せぬエラーが発生しました。管理者にお問い合わせください</label></br>
-    <button type="button" onclick="location.href='{{route("menu.user")}}'">メニューへ</button>
-</body>
-
-</html>
+@section('content')
+<label>予期せぬエラーが発生しました。管理者にお問い合わせください</label></br>
+<button type="button" onclick="location.href='{{route("menu.user")}}'">メニューへ</button>
+@endsection
